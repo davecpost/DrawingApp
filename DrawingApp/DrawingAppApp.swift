c778f5876868046ed83164459d1164b5ff258410
David Post <davecpost@gmail.com>
2021-10-22 14:15:05 -0600

    initial commit

---
+++ DrawingApp/DrawingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingAppApp.swift
+//  DrawingApp
+//
+//  Created by David Post on 2021-10-22.
+//
+
+import SwiftUI
+
+@main
+struct DrawingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
